b3a0eb28dc3d8a792f324d162bf1f768942bfc2b
KAI1902 <kaibineng528@gmail.com>
2024-09-06 17:13:50 +0800

    Update G80.tsx

--- AssFolder/MapPages/G80.tsx
+++ AssFolder/MapPages/G80.tsx
@@ -1,44 +1,41 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Image, TouchableOpacity, Text } from "react-native";
-
+import { View, StyleSheet, Image, TouchableOpacity, Text, ScrollView } from "react-native";
 
 const G80 = () => {
   const [imageSource, setImageSource] = useState(require('./MapImg/G80.png'));
 
   return (
     <View style={styles.container}>
-      <Image source={imageSource} style={styles.image} />
-      
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.scrollViewContent}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Image source={imageSource} style={styles.image} />
+      </ScrollView>
+
       <View style={styles.tabContainer}>
         <TouchableOpacity 
           style={styles.button}
-          onPress={() => {
-            setImageSource(require('./MapImg/G80.png'));
-          }}
+          onPress={() => setImageSource(require('./MapImg/G80.png'))}
         >
           <Text style={styles.buttonText}>G</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
-          onPress={() => {
-            setImageSource(require('./MapImg/L1.png'));
-          }}
+          onPress={() => setImageSource(require('./MapImg/L1.png'))}
         >
           <Text style={styles.buttonText}>F1</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
-          onPress={() => {
-            setImageSource(require('./MapImg/L2.png'));
-          }}
+          onPress={() => setImageSource(require('./MapImg/L2.png'))}
         >
           <Text style={styles.buttonText}>F2</Text>
         </TouchableOpacity>
         <TouchableOpacity 
           style={styles.button}
-          onPress={() => {
-            setImageSource(require('./MapImg/L3.png'));
-          }}
+          onPress={() => setImageSource(require('./MapImg/L3.png'))}
         >
           <Text style={styles.buttonText}>F3</Text>
         </TouchableOpacity>
@@ -52,6 +49,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
+  scrollViewContent: {
+    width: '200%', 
+    paddingLeft: 70,
+  },
   image: {
     width: '100%',
     height: '100%',
@@ -59,24 +60,23 @@ const styles = StyleSheet.create({
   },
   tabContainer: {
     position: 'absolute',
-    top: 140, // Adjust this value to move the tab higher
+    top: 140,
     left: 10,
-    width: 55, // Adjust the width to control the width of the buttons
-    // Use flex to arrange buttons vertically
+    width: 55,
     flexDirection: 'column',
     alignItems: 'center',
   },
   button: {
     backgroundColor: '#007bff',
-    padding: 15, // Increase padding for larger buttons
+    padding: 15,
     marginBottom: 10,
     borderRadius: 10,
-    width: '100%', // Make buttons take up the full width of the tabContainer
-    alignItems: 'center', // Center text inside button
+    width: '100%',
+    alignItems: 'center',
   },
   buttonText: {
     color: 'white',
-    fontSize: 18, // Increase font size for better readability
+    fontSize: 18,
   },
 });
 
