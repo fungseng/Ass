3512b7f7922b82f2be7c5878466ec21134d3951f
KAI1902 <kaibineng528@gmail.com>
2024-09-09 21:01:09 +0800

    Update L164.tsx

--- AssFolder/MapPages/L164.tsx
+++ AssFolder/MapPages/L164.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Image, TouchableOpacity, Text } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity, Text,ScrollView } from "react-native";
 
 
-const L164 = () => {
-  const [imageSource, setImageSource] = useState(require('./MapImg/L164.png'));
+const L163 = () => {
+  const [imageSource, setImageSource] = useState(require('./MapImg/L163.png'));
 
   return (
     <View style={styles.container}>
-      <Image source={imageSource} style={styles.image} />
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.scrollViewContent}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Image source={imageSource} style={styles.image} />
+      </ScrollView>
       <View style={styles.tabContainer}>
         <TouchableOpacity style={styles.button}
           onPress={() => {setImageSource(require('./MapImg/G.png'));
@@ -17,7 +23,7 @@ const L164 = () => {
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}
-          onPress={() => {setImageSource(require('./MapImg/L164.png'));
+          onPress={() => {setImageSource(require('./MapImg/L163.png'));
           }}>
           <Text style={styles.buttonText}>F1</Text>
         </TouchableOpacity>
@@ -38,11 +44,15 @@ const L164 = () => {
     </View>
   );
 };
-export default L164;
+export default L163;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  scrollViewContent: {
+    width: '200%', // Adjust this value based on the width of your images or content
+    paddingLeft: 70,
   },
   image: {
     width: '100%',
@@ -51,25 +61,22 @@ const styles = StyleSheet.create({
   },
   tabContainer: {
     position: 'absolute',
-    top: 140, // Adjust this value to move the tab higher
+    top: 140,
     left: 10,
-    width: 55, // Adjust the width to control the width of the buttons
-    // Use flex to arrange buttons vertically
+    width: 55,
     flexDirection: 'column',
     alignItems: 'center',
   },
   button: {
     backgroundColor: '#007bff',
-    padding: 15, // Increase padding for larger buttons
+    padding: 15,
     marginBottom: 10,
     borderRadius: 10,
-    width: '100%', // Make buttons take up the full width of the tabContainer
-    alignItems: 'center', // Center text inside button
+    width: '100%',
+    alignItems: 'center',
   },
   buttonText: {
     color: 'white',
-    fontSize: 18, // Increase font size for better readability
+    fontSize: 18,
   },
 });
-
-
