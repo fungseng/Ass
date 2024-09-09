5807d3d632a08fa604c2294955e422effb829498
KAI1902 <kaibineng528@gmail.com>
2024-09-09 21:03:16 +0800

    Update L166.tsx

--- AssFolder/MapPages/L166.tsx
+++ AssFolder/MapPages/L166.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Image, TouchableOpacity, Text } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity, Text,ScrollView } from "react-native";
 
 
 const L166 = () => {
@@ -7,7 +7,13 @@ const L166 = () => {
 
   return (
     <View style={styles.container}>
-      <Image source={imageSource} style={styles.image} />
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.scrollViewContent}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Image source={imageSource} style={styles.image} />
+      </ScrollView>
       <View style={styles.tabContainer}>
         <TouchableOpacity style={styles.button}
           onPress={() => {setImageSource(require('./MapImg/G.png'));
@@ -44,6 +50,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
+  scrollViewContent: {
+    width: '200%', // Adjust this value based on the width of your images or content
+    paddingLeft: 70,
+  },
   image: {
     width: '100%',
     height: '100%',
@@ -51,24 +61,23 @@ const styles = StyleSheet.create({
   },
   tabContainer: {
     position: 'absolute',
-    top: 140, // Adjust this value to move the tab higher
+    top: 140,
     left: 10,
-    width: 55, // Adjust the width to control the width of the buttons
-    // Use flex to arrange buttons vertically
+    width: 55,
     flexDirection: 'column',
     alignItems: 'center',
   },
   button: {
     backgroundColor: '#007bff',
-    padding: 15, // Increase padding for larger buttons
+    padding: 15,
     marginBottom: 10,
     borderRadius: 10,
-    width: '100%', // Make buttons take up the full width of the tabContainer
-    alignItems: 'center', // Center text inside button
+    width: '100%',
+    alignItems: 'center',
   },
   buttonText: {
     color: 'white',
-    fontSize: 18, // Increase font size for better readability
+    fontSize: 18,
   },
 });
 
